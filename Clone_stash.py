a658dd4289c0af223b8f1e238a733038c8bd9e6b
Gooch <stephanie.gooch@kpmg.co.uk>
2019-02-27 09:34:48 +0000

    clone stash

---
+++ Clone_stash.py
@@ -0,0 +1,16 @@
+
+import stashy
+import sys
+import os
+
+
+stash = stashy.connect("https://stash.customappsteam.co.uk/", "", "")
+pro = stash.projects.list()
+
+vod  = stash.projects[''].repos.list()
+    
+for repo in vod:
+    for link in repo['links']['clone']:
+        if link['name'] == 'http':
+            os.system("git clone %s" % link['href'])
+            break
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Tools
+
+Clone_stash - from https://github.com/cosmin/stashy
+Clone all repos in stash
+
